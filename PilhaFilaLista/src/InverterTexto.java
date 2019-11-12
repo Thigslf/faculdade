07cf61242c6402ae3fb5f05459944bd0eeaa524d
Thigslf <47948301+Thigslf@users.noreply.github.com>
2019-11-12 15:04:03 -0300

    Add files via upload

---
+++ PilhaFilaLista/src/InverterTexto.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class InverterTexto {
+    public static void main(String[] args){
+        int d,x,ctr,n;
+        NodeStack s = new NodeStack();
+        Scanner in = new Scanner(System.in);
+        n= in.nextInt();
+        ctr=0;
+        x=n;
+        d=2;
+    } 
+    for (int i=0, i< ctr, i++)
+        System.out.print("" + s.pop());
+        System.out.println();
+}
